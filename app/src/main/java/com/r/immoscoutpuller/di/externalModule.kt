e4e9a027a66f9af096fef87abb5c335c4329a34c
Roman Vysotsky <roman.vysotsky@zelfi.com>
2020-07-16 17:48:13 +0200

    fix compile issues

--- app/src/main/java/com/r/immoscoutpuller/di/externalModule.kt
+++ app/src/main/java/com/r/immoscoutpuller/di/externalModule.kt
@@ -1,19 +1,15 @@
 package com.r.immoscoutpuller.di
 
-import com.r.immoscoutpuller.R
 import com.r.immoscoutpuller.immoscout.ImmoScoutParser
 import com.r.immoscoutpuller.immoscout.ImmoScoutParserImpl
 import com.r.immoscoutpuller.notifications.NotificationRepository
 import com.r.immoscoutpuller.notifications.NotificationRepositoryImpl
 import com.r.immoscoutpuller.repository.ImmoRepository
-import com.r.immoscoutpuller.repository.ImmoScoutWebRepository
 import com.r.immoscoutpuller.repository.ImmoUrlRepository
 import com.r.immoscoutpuller.repository.WorkRepository
 import com.r.immoscoutpuller.repository.impl.ImmoRepositoryImpl
 import com.r.immoscoutpuller.repository.impl.ImmoUrlRepositoryImpl
 import com.r.immoscoutpuller.repository.impl.WorkRepositoryImpl
-import com.roman.basearch.repository.WebRepository
-import com.roman.basearch.repository.WebRepositoryFactory
 import org.koin.dsl.module
 
 /**
@@ -23,16 +19,6 @@ import org.koin.dsl.module
  */
 
 val externalModule = module {
-
-    single {
-        WebRepositoryFactory()
-            .create(
-                WebRepository(
-                    get(),
-                    R.string.immo_base_url,
-                    ImmoScoutWebRepository::class.java)
-            )
-    }
 
     single<ImmoRepository> { ImmoRepositoryImpl() }
 
