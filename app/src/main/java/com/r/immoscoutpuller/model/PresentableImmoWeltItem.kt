fb09f28daf67815fe68a6ab6cf4d134c07a2a296
Roman Vysotsky <roman.vysotsky@zelfi.com>
2020-07-16 21:35:14 +0200

    map the immo welt pojo to the PresentableImmoWeltItem.kt properties

--- app/src/main/java/com/r/immoscoutpuller/model/PresentableImmoWeltItem.kt
+++ app/src/main/java/com/r/immoscoutpuller/model/PresentableImmoWeltItem.kt
@@ -1,5 +1,6 @@
 package com.r.immoscoutpuller.model
 
+import com.r.immoscoutpuller.R
 import com.r.immoscoutpuller.immowelt.model.ImmoWeltItemResponse
 import com.roman.basearch.utility.TextLocalization
 import com.roman.basearch.viewmodel.getKoinInstance
@@ -15,15 +16,25 @@ class PresentableImmoWeltItem(val pojo: ImmoWeltItemResponse): ImmoItem() {
     @Transient
     private val textLocalization: TextLocalization = getKoinInstance()
 
-    override val id = ""
-    override val dataTypeHashCode: Int = 1
+    override val id = pojo.id.toString()
+    override val dataTypeHashCode: Int = pojo.hashCode()
 
-    override val warmRent = ""
-    override val rooms = ""
-    override val livingSpace = ""
+    override val title = pojo.title
+    override val warmRent = pojo.price+" EUR +"
+    override val rooms = textLocalization.getString(R.string.item_rooms, pojo.rooms)
+    override val livingSpace = textLocalization.getString(R.string.item_living_space, pojo.livingSpace)
 
-    override val title = ""
-    override val inserted = ""
-    override val lastModified = ""
+
+    override val inserted =
+        textLocalization.getString(
+            R.string.item_inserted,
+            textLocalization.getString(R.string.common_unknown)
+        )
+
+    override val lastModified =
+        textLocalization.getString(
+            R.string.item_last_modified,
+            textLocalization.getString(R.string.common_unknown)
+        )
 
 }
