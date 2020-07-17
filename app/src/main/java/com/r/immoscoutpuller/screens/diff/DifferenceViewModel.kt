bc5835fa28962589ee052918926cec3d1b29b51b
Roman Vysotsky <roman.vysotsky@zelfi.com>
2020-07-17 03:29:10 +0200

    get diffs from local repo

--- app/src/main/java/com/r/immoscoutpuller/screens/diff/DifferenceViewModel.kt
+++ app/src/main/java/com/r/immoscoutpuller/screens/diff/DifferenceViewModel.kt
@@ -1,9 +1,15 @@
 package com.r.immoscoutpuller.screens.diff
 
 import androidx.lifecycle.MutableLiveData
+import com.r.immoscoutpuller.R
 import com.r.immoscoutpuller.background.ImmoListDiffer
+import com.r.immoscoutpuller.immoscout.DIFF_PREFIX
 import com.r.immoscoutpuller.model.ImmoItem
+import com.roman.basearch.utility.LocalRepository
+import com.roman.basearch.utility.TextLocalization
 import com.roman.basearch.viewmodel.BaseViewModel
+import com.roman.basearch.viewmodel.launch
+import org.koin.core.inject
 
 /**
  *
@@ -14,6 +20,9 @@ import com.roman.basearch.viewmodel.BaseViewModel
 class DifferenceViewModel : BaseViewModel() {
 
     val diffItems: MutableLiveData<List<ImmoListDiffer.Diff<ImmoItem>>> = MutableLiveData()
+
+    private val localRepository: LocalRepository by inject()
+    private val textLocalization: TextLocalization by inject()
 
 
     init {
@@ -30,6 +39,20 @@ class DifferenceViewModel : BaseViewModel() {
 
     private fun fetchAllDiffItems() {
 
+        val getItemsFlow = localRepository
+            .readAllWithPrefix<ImmoListDiffer.Diff<ImmoItem>>(DIFF_PREFIX)
+
+        launch(
+            getItemsFlow,
+            { diffItems.postValue(it) },
+            { handleErrorOnReadDiffs(it) }
+        )
+    }
+
+    private fun handleErrorOnReadDiffs(error: Throwable) {
+        val errorMessage = textLocalization.getString(R.string.diff_error, error.toString())
+        message.postValue(errorMessage)
+        this.error.postValue(error)
     }
 
 }
